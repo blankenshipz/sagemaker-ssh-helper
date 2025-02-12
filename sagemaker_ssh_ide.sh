485b7d828134acbc182b0414209b98907d0534c4
Zach Blankenship <zach.blankenship@gmail.com>
2025-02-12 16:14:44 +0000

    Add simplified SSH instructions for port forwarding

---
+++ sagemaker_ssh_ide.sh
@@ -0,0 +1,58 @@
+check_password_set() {
+    local user=${1:-$(whoami)}
+    passwd -S "$user" 2>/dev/null | grep -q '^[^L]*P'
+    return $?
+}
+
+current_user=$(whoami)
+
+if ! check_password_set "$current_user"; then
+    echo "No password set for $current_user"
+    # Prompt for new password securely (hidden input)
+    read -s -p "Enter new password: " password
+    echo
+    read -s -p "Confirm password: " password2
+    echo
+    
+    if [ "$password" = "$password2" ]; then
+        # Use chpasswd to set password non-interactively
+        echo "$current_user:$password" | sudo chpasswd
+        if [ $? -eq 0 ]; then
+            echo "Password set successfully"
+        else
+            echo "Failed to set password"
+            exit 1
+        fi
+    else
+        echo "Passwords do not match"
+        exit 1
+    fi
+else
+    echo "Password is already set for $current_user"
+fi
+
+# Configure sm-ssh-ide
+sudo ./sagemaker_ssh_helper/sm-ssh-ide configure --ssh-only
+
+# Init SSM
+sudo \
+    AWS_CONTAINER_CREDENTIALS_RELATIVE_URI="$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI" \
+    AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION" \
+    AWS_REGION="$AWS_REGION" \
+    AWS_INTERNAL_IMAGE_OWNER="$AWS_INTERNAL_IMAGE_OWNER" \
+    AWS_ACCOUNT_ID="$AWS_ACCOUNT_ID" \
+    ./sagemaker_ssh_helper/sm-ssh-ide init-ssm
+
+# Update the SSHD Config to Allow Password Authentication
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+# Clear Services (Should Start SSHD in the background)
+sudo ./sagemaker_ssh_helper/sm-ssh-ide stop
+sudo ./sagemaker_ssh_helper/sm-ssh-ide start
+
+# Output the Managed Instance ID to the Log
+MANAGED_INSTANCE_ID=$(sudo cat /var/lib/amazon/ssm/registration | jq .ManagedInstanceID)
+echo "!!!!!!!!! Please Note that the Managed Instance ID is ${MANAGED_INSTANCE_ID} !!!!!!!!!!!"
+
+# Start the Agent
+./sagemaker_ssh_helper/sm-ssh-ide ssm-agent
---
+++ sagemaker_ssh_ide_simple_readme.md
@@ -0,0 +1,36 @@
+###
+
+1. Run the following script to setup the notebook node. You will be prompted to set a password for your user, this is the password that you'll use for SSH Connections.
+
+```
+./sagemaker_ssh_ide.sh
+```
+
+Capture the output for the "Managed Instance ID - you should see this logline
+
+> Please Note that the Managed Instance ID is "mi-xxxxxxxxxxxxxxx"
+
+Keep the command running to run the SSH server and SSM Agent. 
+
+2. Setup your local SSH Config with an entry for localhost - we'll forward this port to the notebook in the next step.
+
+Edit `~/.ssh/config` include an entry like the following:
+
+```
+Host sagemaker-notebook
+    HostName localhost
+    Port 2222
+    User sagemaker-user
+```
+
+3. In a terminal run the following command (keep it running) to forward traffic from your local instance to the notebook SSH server over SSM. Make sure to update *$YOUR_MANAGED_INSTANCE_ID*
+
+```
+aws ssm start-session \
+    --target $YOUR_MANAGED_INSTANCE_ID  \
+    --document-name AWS-StartPortForwardingSession \
+    --parameters '{"portNumber":["22"],"localPortNumber":["2222"]}' \
+    --region us-east-1
+```
+
+4. Setup your editor to work with the remote SSH Session. In VSCode there's an extension called "Remote - SSH". *Optionally* setup a custom shell command to make starting the `aws ssm start-session` locally easier.
